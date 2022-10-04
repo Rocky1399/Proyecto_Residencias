497ba6e5cb12dfc387fb0b95f77ed85661cd8314
Rocky1399 <98731281+Rocky1399@users.noreply.github.com>
2022-10-03 18:12:25 -0700

    update

--- ProyectoR/ProyectoR/ProyectoR/Estudiantes/Anteproyecto.aspx.cs
+++ ProyectoR/ProyectoR/ProyectoR/Estudiantes/Anteproyecto.aspx.cs
@@ -51,6 +51,7 @@ namespace ProyectoR.Estudiantes
                             btnUpload.Visible = true;
                         }
                     }
+                    con.Close();
                 }
             }
         }
--- ProyectoR/ProyectoR/ProyectoR/Estudiantes/Revision1.aspx.cs
+++ ProyectoR/ProyectoR/ProyectoR/Estudiantes/Revision1.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -19,6 +22,10 @@ namespace ProyectoR.Estudiantes
             {
                 Response.Redirect("/Login.aspx");
             }
+            if (!this.IsPostBack)
+            {
+                this.BindGrid();
+            }
         }
 
         protected void BtnCerrar_Click(object sender, EventArgs e)
@@ -26,6 +33,95 @@ namespace ProyectoR.Estudiantes
             Session.Remove("Usuario");
             Session.Remove("ID");
             Response.Redirect("/Login.aspx");
+        }
+
+        private void BindGrid()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.CommandText = "SELECT Id, Name FROM tb_revision1 WHERE Id_alumno='" + Session["ID"].ToString() + "'";
+                    cmd.Connection = con;
+                    con.Open();
+                    gvFiles.DataSource = cmd.ExecuteReader();
+                    gvFiles.DataBind();
+                    con.Close();
+                }
+            }
+        }
+
+        protected void Upload(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                if (".pdf" == System.IO.Path.GetExtension(FileUpload1.FileName))
+                {
+                    string filename = Path.GetFileName(FileUpload1.PostedFile.FileName);
+                    string contentType = FileUpload1.PostedFile.ContentType;
+                    using (Stream fs = FileUpload1.PostedFile.InputStream)
+                    {
+                        using (BinaryReader br = new BinaryReader(fs))
+                        {
+                            byte[] bytes = br.ReadBytes((Int32)fs.Length);
+                            string constr = ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
+                            using (SqlConnection con = new SqlConnection(constr))
+                            {
+                                string query = "INSERT INTO tb_revision1 (Name,ContentType,Data,Id_alumno) VALUES (@Name, @ContentType, @Data, @Id_alumno)";
+                                using (SqlCommand cmd = new SqlCommand(query))
+                                {
+                                    cmd.Connection = con;
+                                    cmd.Parameters.AddWithValue("@Name", filename);
+                                    cmd.Parameters.AddWithValue("@ContentType", contentType);
+                                    cmd.Parameters.AddWithValue("@Data", bytes);
+                                    cmd.Parameters.AddWithValue("@Id_alumno", Session["ID"].ToString());
+                                    con.Open();
+                                    cmd.ExecuteNonQuery();
+                                    con.Close();
+                                }
+                            }
+                        }
+                    }
+                    Response.Redirect(Request.Url.AbsoluteUri);
+                }
+                else
+                {
+                    Response.Write("<script>alert('Debe ingresar un archivo PDF');</script>");
+                }
+            }
+            else
+            {
+                Response.Write("<script>alert('Debe ingresar un archivo PDF');</script>");
+            }
+        }
+
+        [System.Web.Services.WebMethod]
+        public static object GetPDF(int fileId)
+        {
+            byte[] bytes;
+            string fileName, contentType;
+            string constr = ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.CommandText = "SELECT Name, Data, ContentType FROM tb_revision1 WHERE Id = @Id";
+                    cmd.Parameters.AddWithValue("@Id", fileId);
+                    cmd.Connection = con;
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        sdr.Read();
+                        bytes = (byte[])sdr["Data"];
+                        contentType = sdr["ContentType"].ToString();
+                        fileName = sdr["Name"].ToString();
+                    }
+                    con.Close();
+                }
+            }
+
+            return new { FileName = fileName, ContentType = contentType, Data = bytes };
         }
     }
 }
