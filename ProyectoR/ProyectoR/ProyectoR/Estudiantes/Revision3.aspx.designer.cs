c1bd5b58295e8e7fce3f65f509356c116669073a
Rocky1399 <98731281+Rocky1399@users.noreply.github.com>
2022-11-28 14:41:50 -0700

    Revisiones
    Arreglado el boton de liberacion, revisiones, etc

--- ProyectoR/ProyectoR/ProyectoR/Estudiantes/Revision3.aspx.designer.cs
+++ ProyectoR/ProyectoR/ProyectoR/Estudiantes/Revision3.aspx.designer.cs
@@ -78,12 +78,12 @@ namespace ProyectoR.Estudiantes
         protected global::System.Web.UI.WebControls.Label LabelComentario;
 
         /// <summary>
-        /// Control Label1.
+        /// Control LabelCalificacion.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
+        protected global::System.Web.UI.WebControls.Label LabelCalificacion;
     }
 }
